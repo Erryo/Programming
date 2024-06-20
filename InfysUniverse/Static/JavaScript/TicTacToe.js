e92c7f93f40064880396893bb6a8999a3485d784
Erryo <159823096+Erryo@users.noreply.github.com>
2024-06-20 21:26:40 +0300

    redid the TicTacToe js

--- InfysUniverse/Static/JavaScript/TicTacToe.js
+++ InfysUniverse/Static/JavaScript/TicTacToe.js
@@ -1,9 +1,10 @@
 let Curr_Player = 0;
 let x_score = 0;
 let o_score = 0;
+let players = ["O", "X"];
 function setButton(id) {
   let header = document.getElementById(id);
-  if (header.value == "_") {
+  if (!players.includes(header.innerHTML)) {
     if (Curr_Player == 0) {
       header.innerHTML = "O";
       Curr_Player = 1;
@@ -14,92 +15,121 @@ function setButton(id) {
       Curr_Player = 0;
     }
   }
-  CheckEnd();
+  if (!checkHoriz()) {
+    if (!checkVert()) {
+      checkDiago();
+    }
+  }
+  document.getElementById("x_sc").value = x_score;
+  document.getElementById("o_sc").value = o_score;
 }
-function CheckEnd() {
-  const horizontal = [1, 4, 7];
-  const vertical = [1, 2, 3];
-  for (let index in horizontal) {
-    const i = horizontal[index];
-    console.log(i);
-    if (document.getElementById(i).value != "_") {
-      if (
-        document.getElementById(i).value == document.getElementById(i + 1).value
-      ) {
-        if (
-          document.getElementById(i).value ==
-          document.getElementById(i + 2).value
-        ) {
-          disable_all();
-          if (document.getElementById(i).value == "X") {
-            x_score++;
 
-            document.getElementById("x_sc").innerHTML = x_score;
-          } else {
-            o_score++;
-            document.getElementById("o_sc").innerHTML = o_score;
-          }
-        }
-      }
+function checkHoriz() {
+  console.log("H");
+  const horiz = [1, 4, 7];
+  for (i of horiz) {
+    if (!players.includes(document.getElementById(i).value)) {
+      continue;
     }
-  }
-  for (let i in vertical) {
-    const num = vertical[i];
-    if (document.getElementById(num).value != "_") {
+    if (
+      document.getElementById(i).value == document.getElementById(i + 1).value
+    ) {
       if (
-        document.getElementById(num).value ==
-        document.getElementById(num + 3).value
+        document.getElementById(i).value == document.getElementById(i + 2).value
       ) {
-        if (
-          document.getElementById(num).value ==
-          document.getElementById(num + 6).value
-        ) {
-          disable_all();
-          if (document.getElementById(num).value == "X") {
-            x_score++;
-            document.getElementById("x_sc").innerHTML = x_score;
-          } else {
-            o_score++;
-            document.getElementById("o_sc").innerHTML = o_score;
-          }
-        }
-      }
-    }
-  }
-
-  if (document.getElementById(1).value != "_") {
-    if (document.getElementById(1).value == document.getElementById(5).value) {
-      if (
-        document.getElementById(1).value == document.getElementById(9).value
-      ) {
+        document
+          .getElementById(i)
+          .classList.replace("grid-btn", "grid-btn-red");
+        document
+          .getElementById(i + 1)
+          .classList.replace("grid-btn", "grid-btn-red");
+        document
+          .getElementById(i + 2)
+          .classList.replace("grid-btn", "grid-btn-red");
         disable_all();
-        if (document.getElementById(1).value == "X") {
+        if (document.getElementById(i).value == "X") {
           x_score++;
-          document.getElementById("x_sc").innerHTML = x_score;
-        } else {
-          o_score++;
-          document.getElementById("o_sc").innerHTML = o_score;
+          return true;
         }
+        o_score++;
+        return true;
       }
     }
   }
-  if (document.getElementById(3).value != "_") {
-    if (document.getElementById(3).value == document.getElementById(5).value) {
-      if (
-        document.getElementById(3).value == document.getElementById(7).value
-      ) {
-        disable_all();
-        if (document.getElementById(3).value == "X") {
-          x_score++;
-          document.getElementById("x_sc").innerHTML = x_score;
-        } else {
-          o_score++;
-          document.getElementById("o_sc").innerHTML = o_score;
-        }
-      }
-    }
-  }
+  return false;
 }
+
+function checkVert() {
+  console.log("V");
+  const verti = [1, 2, 3];
+  for (i of verti) {
+    if (!players.includes(document.getElementById(i).value)) {
+      continue;
+    }
+    if (
+      document.getElementById(i).value == document.getElementById(i + 3).value
+    ) {
+      if (
+        document.getElementById(i).value == document.getElementById(i + 6).value
+      ) {
+        document
+          .getElementById(i)
+          .classList.replace("grid-btn", "grid-btn-red");
+        document
+          .getElementById(i + 3)
+          .classList.replace("grid-btn", "grid-btn-red");
+        document
+          .getElementById(i + 6)
+          .classList.replace("grid-btn", "grid-btn-red");
+        disable_all();
+        if (document.getElementById(i).value == "X") {
+          x_score++;
+          return true;
+        }
+        o_score++;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function checkDiago() {
+  console.log("D");
+  if (!players.includes(document.getElementById(5).value)) {
+    return false;
+  }
+  if (document.getElementById(1).value == document.getElementById(5).value) {
+    if (document.getElementById(1).value == document.getElementById(9).value) {
+      document.getElementById(1).classList.replace("grid-btn", "grid-btn-red");
+      document.getElementById(5).classList.replace("grid-btn", "grid-btn-red");
+      document.getElementById(9).classList.replace("grid-btn", "grid-btn-red");
+      disable_all();
+      if (document.getElementById(1).value == "X") {
+        x_score++;
+        return true;
+      }
+      o_score++;
+      return true;
+    }
+  }
+  if (document.getElementById(3).value == document.getElementById(5).value) {
+    if (document.getElementById(3).value == document.getElementById(7).value) {
+      document.getElementById(3).classList.replace("grid-btn", "grid-btn-red");
+      document.getElementById(5).classList.replace("grid-btn", "grid-btn-red");
+      document.getElementById(7).classList.replace("grid-btn", "grid-btn-red");
+      disable_all();
+      if (document.getElementById(1).value == "X") {
+        x_score++;
+        return true;
+      }
+      o_score++;
+      return true;
+    }
+  }
+  return false;
+}
+
 function disable_all() {
   for (let i = 1; i <= 9; i++) {
     document.getElementById(i).disabled = true;
@@ -110,5 +140,6 @@ function reset() {
     document.getElementById(i).innerHTML = "_";
     document.getElementById(i).value = "_";
     document.getElementById(i).disabled = false;
+    document.getElementById(i).classList.replace("grid-btn-red", "grid-btn");
   }
 }
