e9d0c154d8af0569e07f4a13351a3eb29a80d0c5
Erryo <159823096+Erryo@users.noreply.github.com>
2024-09-14 16:26:04 +0200

    Test to try git merge

--- WaveFunctionCollapse/wfc_v2/main.go
+++ WaveFunctionCollapse/wfc_v2/main.go
@@ -1,6 +1,19 @@
-package wfcv2
+package main
+
+import "fmt"
+
+var numberToIcon map[uint8]string = map[uint8]string{
+	0: "",
+	1: "",
+	2: "",
+	3: "",
+	4: "",
+	5: "",
+}
 
 func main() {
+	box := createBox(7, 5)
+	drawBox(box)
 	// Array of values - mat
 	// Dictionary to draw
 	//
@@ -20,4 +33,13 @@ func createBox(width, length uint8) [][]uint8 {
 		box = append(box, row)
 	}
 	return box
+}
+
+func drawBox(box [][]uint8) {
+	for _, row := range box {
+		for _, cell := range row {
+			fmt.Print(cell)
+		}
+		fmt.Println()
+	}
 }
